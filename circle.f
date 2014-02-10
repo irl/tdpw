d8d9621f37c8fdfd95c50cff885cb021c3f9babc
Iain R. Learmonth <irl@fsfe.org>
2014-02-10 18:13:27 +0000

    Added LICENSE

--- circle.f
+++ circle.f
@@ -1,3 +1,20 @@
+C tdpw - Two Dimensional Physics Workbench
+C Copyright (C) Iain R. Learmonth 2012-2014.
+
+C This program is free software; you can redistribute it and/or modify
+C it under the terms of the GNU General Public License as published by
+C the Free Software Foundation; either version 2 of the License, or
+C (at your option) any later version.
+C
+C This program is distributed in the hope that it will be useful,
+C but WITHOUT ANY WARRANTY; without even the implied warranty of
+C MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+C GNU General Public License for more details.
+C
+C You should have received a copy of the GNU General Public License along
+C with this program; if not, write to the Free Software Foundation, Inc.,
+C 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 C Does the circle maths
 
       INTEGER FUNCTION INCIRC(X,Y,R)
--- tools/mkobf.c
+++ tools/mkobf.c
@@ -1,3 +1,21 @@
+/**
+ * tdpw - Two Dimensional Physics Workbench
+ * Copyright (C) Iain R. Learmonth 2012-2014.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #include <stdio.h>
 #include <math.h>
@@ -24,8 +42,8 @@ int main()
         for ( y = 20 ; y < 320 ; y += ( rand() % 20 ) + 10 )
                 printf("shape 590 %d %d 0 0 random\n", y, ( rand() % 10 ) + 10);
 
-	for ( x = 10 ; x < 610 ; x += ( rand() % 20 ) + 10 ) 
-		for ( y = 10 ; y < 330 ; y += ( rand() % 20 ) + 10 ) 
+	for ( x = 10 ; x < 610 ; x += ( rand() % 20 ) + 10 )
+		for ( y = 10 ; y < 330 ; y += ( rand() % 20 ) + 10 )
 			printf("shape %d %d %d 2 2 random\n",
 				(rand() % 630), (rand() % 300), (rand() % 20) + 10);
 
